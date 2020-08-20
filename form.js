82371149eb6986fc2ce35ede89fb3dab1b909b8f
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-08-20 17:36:35 -0400

    change cuda versions depending on the cluster

--- form.js
+++ form.js
@@ -12,6 +12,15 @@ function clamp(min, max, val) {
 }
 
 /**
+ * Simple helper to return the capitalized version of the
+ * current select cluster (i.e., Owens and Pitzer).
+ */
+function current_cluster(){
+  var cluster = $('#batch_connect_session_context_cluster').val();
+  return cluster.charAt(0).toUpperCase() + cluster.slice(1);
+}
+
+/**
  * Fix num cores, allowing blanks to remain
  */
 function fix_num_cores() {
@@ -33,8 +42,7 @@ function fix_num_cores() {
  */
 function set_ppn_by_node_type(node_type_input, num_cores_input) {
   const data = node_type_input.find(':selected').data();
-  var cluster = $('#batch_connect_session_context_cluster').val();
-  cluster = cluster.charAt(0).toUpperCase() + cluster.slice(1);
+  const cluster = current_cluster();
 
   // classroom deployments don't have node_type_input
   if(!data){
@@ -77,12 +85,40 @@ function toggle_visibility_of_form_group(form_id, show) {
 }
 
 /**
+ * Update the cuda select options because not all options are available
+ * on all clusters. So hide/show them as appropriate.
+ */
+function update_cuda_options() {
+  const cluster = current_cluster();
+  const cuda_options = $('#batch_connect_session_context_cuda_version option');
+
+  cuda_options.each(function(_i, option) {
+    // the variable 'option' is just a data structure. it has no attr, data, show
+    // or hide methods so we have to query for it again
+    let option_element = $("#batch_connect_session_context_cuda_version option[value='" + option.value + "']");
+    let data = option_element.data();
+    let show = data["versionFor" + cluster];
+
+    if(show) {
+      option_element.show();
+    } else {
+      option_element.hide();
+    }
+  });
+}
+
+/**
  * Toggle the visibility of the CUDA select when the selected
  * node_type changes
  */
 function toggle_cuda_version_visibility(selected_node_type) {
   const cuda_element = $('#batch_connect_session_context_cuda_version');
-  toggle_visibility_of_form_group(cuda_element, selected_node_type == 'gpu');
+  const choose_gpu = selected_node_type == 'gpu';
+
+  toggle_visibility_of_form_group(cuda_element, choose_gpu);
+  if(choose_gpu){
+    update_cuda_options();
+  }
 }
 
 /**
@@ -114,7 +150,7 @@ function node_type_change_handler(event) {
  */
 function cluster_change_handler(event) {
   fix_num_cores();
-  toggle_cuda_version_visibility(event.target.value);
+  update_cuda_options();
 }
 
 /**
